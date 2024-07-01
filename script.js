7a0d5333bc5a2b5e49f5252759e2cf6cdc1fa7dd
sardor <alimukhammad1@gmail.com>
2024-07-01 00:59:28 -0400

    1

---
+++ script.js
@@ -0,0 +1,35 @@
+function hideHome(){
+    let home = document.getElementById('home');
+    if(home.style.display === 'block'){
+        home.style.display = 'none';
+    }else{
+        home.style.display = 'block';
+    }
+}
+
+function hideExp(){
+    let exp = document.getElementById('exp');
+    if(exp.style.display === 'block'){
+        exp.style.display = 'none';
+    }else{
+        exp.style.display = 'block';
+    }
+}
+
+function hideProject(){
+    let project = document.getElementById('proj');
+    if(project.style.display === 'block'){
+        project.style.display = 'none';
+    }else{
+        project.style.display = 'block';
+    }
+}
+
+function hideContact(){
+    let contact = document.getElementById('contact');
+    if(contact.style.display === 'block'){
+        contact.style.display = 'none';
+    }else{
+        contact.style.display = 'block';
+    }
+}
